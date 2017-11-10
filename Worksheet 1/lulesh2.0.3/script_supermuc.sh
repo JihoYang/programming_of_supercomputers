fd7c38674c546a0e9dc3e5d34d2fa8141bce6de6
Jiho Yang <yangji@in.tum.de>
2017-11-10 15:26:18 +0100

    updated script to be run on supermuc

--- Worksheet 1/lulesh2.0.3/script_supermuc.sh
+++ Worksheet 1/lulesh2.0.3/script_supermuc.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#@ wall_clock_limit = 00:20:00
+#@ job_name = pos-lulesh-openmp
+#@ job_type = MPICH
+#@ class = test
+#@ output = pos_lulesh_openmp_$(jobid).out
+#@ error = pos_lulesh_openmp_$(jobid).out
+#@ node = 1
+#@ total_tasks = 16
+#@ node_usage = not_shared
+#@ energy_policy_tag = lulesh
+#@ minimize_time_to_solution = yes
+#@ island_count = 1
+#@ queue
+. /etc/profile
+. /etc/profile.d/modules.sh
 
 #Remove output files
 rm noflags.txt
